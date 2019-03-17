a3c917887b16a8fef5ccf7532662c8651fd50dbb
ubuntu <patriciamilou@gmail.com>
2019-03-17 01:43:27 +0000

    Getting some statistics

--- getsignature.py
+++ getsignature.py
@@ -2,85 +2,49 @@ import hashlib
 import hmac
 import base64
 import urllib.request, json
-f= open("data.json","w+")
+
 api_key     = 'V37UcPDfsVDiaYgs'
 secret_key  = b'yOjBov7vc-0op26qkHmaLOL4abzb34oK'
 query       = ('/events?year=2012&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2012       = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
+
 
 query       = ('/events?year=2013&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2013       = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2014&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2014         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2015&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2015         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2016&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2016       = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2017&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2017         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2018&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
-with urllib.request.urlopen(url) as url:
-    i=i+1
-    data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+url2018         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
 
 query       = ('/events?year=2019&key=' + api_key).encode("utf-8")
 signature   = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
-i=-1
+url2019         = 'https://api.edinburghfestivalcity.com' + query.decode() + '&signature=' + signature
+
+
+
+
+import urllib.request
 with urllib.request.urlopen(url) as url:
-    i=i+1
     data = json.loads(url.read().decode())
-    f.write(str(data)+"\n")
-    print(data)
+    for p in data:
+        print('festival id: ' + p)
+        break
+    #print(data)
--- gettopdata.py
+++ gettopdata.py
@@ -1,9 +1,61 @@
 import json
+import datetime
+import time
+date=datetime.datetime.now().strftime('%G-%m-%d %H:%M:%S')
+date = time.strptime(date, '%Y-%m-%d %H:%M:%S')
+print(date)
 
-with open('data.txt') as json_file:
-    data = json.load(json_file)
-    for p in data:
-        # print('Name: ' + p['name'])
-        # print('Website: ' + p['website'])
-        # print('From: ' + p['from'])
-        print('')
+genres={}
+performances={}
+discounts={}
+agecat={}
+upcoming={}
+files=["data_2012.txt","data_2013.txt","data_2014.txt","data_2015.txt","data_2016.txt","data_2017.txt","data_2018.txt","data_2019.txt"]
+for file in files:
+    print("Reading file: "+file)
+    with open(file) as json_file:
+        data = json.load(json_file)
+        for p in data:
+            if( p["genre"] in genres):
+                counter= genres[p["genre"]]+1
+                genres[p["genre"]]=counter
+            else:
+                genres[p["genre"]]=1
+
+            for x in p["performances"]:
+                avshows={}
+                i=0
+                if(file== "data_2019.txt"):
+                    i=i+1
+                    date2=time.strptime(x["start"], '%Y-%m-%d %H:%M:%S')
+                    if(date2>date):
+                        avshows[p["title"]+"_"+str(i)]=x
+                        upcoming[p["genre"]]=avshows
+                if( p["genre"] in performances):
+                    counter= performances[p["genre"]]+1
+                    performances[p["genre"]]=counter
+                else:
+                    performances[p["genre"]]=1
+            discount={}
+            for x in p["discounts"]:
+                if( x in discount  and x== True ):
+                    counter= discount[x]+1
+                    discount[x]=counter
+                else:
+                    discount[x]=1
+            discounts[p["genre"]]=discount
+            if(file)== "data_2019.txt":
+                agecat[p["title"]]=p["age_category"]
+
+
+
+print("Genres:")
+print(genres)
+print("Performances:")
+print(performances)
+print("Discounts:")
+print(discounts)
+print("Age Categories:")
+print(agecat)
+print("Upcoming")
+print(upcoming)
